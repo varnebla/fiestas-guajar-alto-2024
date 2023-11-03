2b5444b599242703cfad5d721d0e2781e6e0662f
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2023-11-03 01:59:25 +0100

    feat: improve seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://fiestasguajaralto2024.com',
+  integrations: [tailwind(), sitemap()]
 });
